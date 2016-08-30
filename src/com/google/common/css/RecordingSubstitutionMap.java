d4476e5295f20780cf453df8e534ceeae991fcb9
msamuel <msamuel@google.com>
2016-08-30 08:45:37 -0700

    Rework substitution maps to allow reusing them across compiles.
    I need to be able to compile multiple CSS bundles using a rename map,
    then recompile just a few based on hash checking.
    
    There are two problems with using CssCompiler.execute(renameFile, ...).
    1. CssCompiler writes out the rename map using only the entries actually
       encountered by keeping a side table of mappings, not getting the
       entire mapping from the SubstitutionMap.
    2. MinimalSubstitutionMap and others have no standard way to reconstitute
       a substitution map from a string->string relation.
    
    This addresses both problems.
    
    I added read() methods to OutputRenamingMapFormat.
    
    I added an Initializable interface to SubstitutionMap that allows
    reconstituting a renamer from a rename map, and reworked
    RecordingSubstitutionMap and friends to implement Initializable.
    
    Along the way, I cleared up an ambiguity in how SplittingSubstitutionMap
    & RecodingSubstitutionMap interpreted renamings that caused problems
    when a prefixing substitution map effectively prefixed only the first
    renaming in a chain.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=131714015

--- src/com/google/common/css/RecordingSubstitutionMap.java
+++ src/com/google/common/css/RecordingSubstitutionMap.java
@@ -19,52 +19,40 @@ package com.google.common.css;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.css.MultipleMappingSubstitutionMap.ValueWithMappings;
-
 import java.util.Map;
-
-import javax.annotation.Nullable;
 
 /**
  * A decorator for a {@link SubstitutionMap} that records which values it maps.
  *
  * @author bolinfest@google.com (Michael Bolin)
  */
-public class RecordingSubstitutionMap implements SubstitutionMap {
+public class RecordingSubstitutionMap implements SubstitutionMap.Initializable {
 
   private final SubstitutionMap delegate;
 
   private final Predicate<? super String> shouldRecordMappingForCodeGeneration;
 
-  private final Map<String,String> mappings;
-
-  /**
-   * If defined, this should be used rather than delegate to look up a mapping.
-   */
-  @Nullable
-  private final MultipleMappingSubstitutionMap multipleMappingSubstitutionMap;
+  // Use a LinkedHashMap so getMappings() is deterministic.
+  private final Map<String, String> mappings = Maps.newLinkedHashMap();
 
   /**
    * Creates a new instance.
+   *
    * @param map A SubstitutionMap decorated by this class
-   * @param shouldRecordMappingForCodeGeneration A predicate that returns true
-   *     if the mapping should be recorded for code-generation purposes
+   * @param shouldRecordMappingForCodeGeneration A predicate that returns true if the mapping should
+   *     be recorded for code-generation purposes
+   * @deprecated Use {@link Builder} instead.
    */
-  public RecordingSubstitutionMap(SubstitutionMap map,
-      Predicate<? super String> shouldRecordMappingForCodeGeneration) {
+  @Deprecated
+  public RecordingSubstitutionMap(
+      SubstitutionMap map, Predicate<? super String> shouldRecordMappingForCodeGeneration) {
     this.delegate = map;
     this.shouldRecordMappingForCodeGeneration =
         shouldRecordMappingForCodeGeneration;
-
-    // Use a LinkedHashMap so getMappings() is deterministic.
-    this.mappings = Maps.newLinkedHashMap();
-
-    this.multipleMappingSubstitutionMap =
-        (map instanceof MultipleMappingSubstitutionMap)
-        ? (MultipleMappingSubstitutionMap)map
-        : null;
   }
 
   /**
@@ -78,24 +66,85 @@ public class RecordingSubstitutionMap implements SubstitutionMap {
       return key;
     }
 
-    if (mappings.containsKey(key)) {
-      return mappings.get(key);
-    } else if (multipleMappingSubstitutionMap != null) {
-      ValueWithMappings valueWithMappings = multipleMappingSubstitutionMap
-          .getValueWithMappings(key);
+    if (delegate instanceof MultipleMappingSubstitutionMap) {
+      // The final value only bears a loose relationship to the mappings.
+      // For example, PrefixingSubstitutionMap applied to a MinimalSubstitutionMap
+      // minimizes all components but only prefixes the first.
+      // We can't memoize the value here, so don't look up in mappings first.
+      ValueWithMappings valueWithMappings =
+          ((MultipleMappingSubstitutionMap) delegate).getValueWithMappings(key);
       mappings.putAll(valueWithMappings.mappings);
       return valueWithMappings.value;
     } else {
-      String value = delegate.get(key);
-      mappings.put(key, value);
+      String value = mappings.get(key);
+      if (value == null) {
+        value = delegate.get(key);
+        mappings.put(key, value);
+      }
       return value;
     }
   }
 
   /**
-   * @return The recorded mappings in the order they were created.
+   * @return The recorded mappings in the order they were created. This output may be used with
+   *     {@link OutputRenamingMapFormat#writeRenamingMap}
    */
-  public Map<String,String> getMappings() {
+  public Map<String, String> getMappings() {
     return ImmutableMap.copyOf(mappings);
+  }
+
+  @Override
+  public void initializeWithMappings(Map<? extends String, ? extends String> newMappings) {
+    Preconditions.checkState(mappings.isEmpty());
+    if (!newMappings.isEmpty()) {
+      mappings.putAll(newMappings);
+      ((SubstitutionMap.Initializable) delegate).initializeWithMappings(newMappings);
+    }
+  }
+
+  /** A-la-carte builder. */
+  public static final class Builder {
+    private SubstitutionMap delegate = new IdentitySubstitutionMap();
+    private Predicate<? super String> shouldRecordMappingForCodeGeneration =
+        Predicates.alwaysTrue();
+    private Map<String, String> mappings = Maps.newLinkedHashMap();
+
+    /** Specifies the underlying map. Multiple calls clobber. */
+    public Builder withSubstitutionMap(SubstitutionMap d) {
+      this.delegate = Preconditions.checkNotNull(d);
+      return this;
+    }
+
+    /**
+     * True keys that should be treated mapped to themselves instead of passing through Multiple
+     * calls AND.
+     */
+    public Builder shouldRecordMappingForCodeGeneration(Predicate<? super String> p) {
+      shouldRecordMappingForCodeGeneration =
+          Predicates.and(shouldRecordMappingForCodeGeneration, p);
+      return this;
+    }
+
+    /**
+     * Specifies mappings to {@linkplain Initializable initialize} the delegate with. Multiple calls
+     * putAll. This can be used to reconstitute a map that was written out by {@link
+     * OutputRenamingMapFormat#writeRenamingMap} from the output of {@link
+     * OutputRenamingMapFormat#readRenamingMap}.
+     */
+    public Builder withMappings(Map<? extends String, ? extends String> m) {
+      this.mappings.putAll(m);
+      return this;
+    }
+
+    /** Builds the substitution map based on previous operations on this builder. */
+    public RecordingSubstitutionMap build() {
+      // TODO(msamuel): if delegate instanceof MultipleMappingSubstitutionMap
+      // should this return a RecordingSubstitutionMap that is itself
+      // a MultipleMappingSubstitutionMap.
+      RecordingSubstitutionMap built =
+          new RecordingSubstitutionMap(delegate, shouldRecordMappingForCodeGeneration);
+      built.initializeWithMappings(mappings);
+      return built;
+    }
   }
 }
---
+++ tests/com/google/common/css/RecordingSubstitutionMapTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.css;
+
+import com.google.common.collect.ImmutableSet;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import junit.framework.AssertionFailedError;
+import junit.framework.TestCase;
+
+public final class RecordingSubstitutionMapTest extends TestCase {
+
+  private static final ImmutableSet<String> OUTPUT_BLACKLIST = ImmutableSet.of("c", "e", "i");
+
+  private static SubstitutionMap createDelegate() {
+    // Test a whole mess of delegate classes.
+    SubstitutionMap m = new MinimalSubstitutionMap(OUTPUT_BLACKLIST);
+    m = new SplittingSubstitutionMap(m);
+    m = new PrefixingSubstitutionMap(m, "x-");
+    return m;
+  }
+
+  public final void testReadAndWrite() throws IOException {
+    for (OutputRenamingMapFormat format : OutputRenamingMapFormat.values()) {
+      RecordingSubstitutionMap recording =
+          new RecordingSubstitutionMap.Builder().withSubstitutionMap(createDelegate()).build();
+      // Put some stuff into the map.
+      // TIL: there are a lot of websites on the A-Z of fruits & vegetables.
+      assertEquals("x-a", recording.get("apple"));
+      assertEquals("x-b", recording.get("banana"));
+      assertEquals("x-d", recording.get("durian"));
+      assertEquals("x-f-g", recording.get("figgy-goop"));
+
+      // Write it out.
+      StringWriter out = new StringWriter();
+      String formatted;
+      try {
+        format.writeRenamingMap(recording.getMappings(), out);
+        formatted = out.toString();
+      } finally {
+        out.close();
+      }
+
+      // Reconstitute it.
+      RecordingSubstitutionMap recordingFromString;
+      StringReader in = new StringReader(formatted);
+      try {
+        recordingFromString =
+            new RecordingSubstitutionMap.Builder()
+                .withSubstitutionMap(createDelegate())
+                .withMappings(format.readRenamingMap(in))
+                .build();
+        assertEquals(-1, in.read());
+      } catch (IOException | RuntimeException ex) {
+        throw (AssertionFailedError)
+            new AssertionFailedError(
+                "Problem with input formatted according to "
+                + format
+                + "\n```\n"
+                + formatted
+                + "\n```")
+            .initCause(ex);
+      } finally {
+        in.close();
+      }
+
+      // Vary the order to check that we get stable values from the
+      // earlier uses, and unambiguous new values.
+      assertEquals("x-b", recordingFromString.get("banana"));
+      assertEquals("x-h", recordingFromString.get("honeydew"));
+      assertEquals("x-a", recordingFromString.get("apple"));
+      assertEquals("x-f-g", recordingFromString.get("figgy-goop"));
+      assertEquals("x-d", recordingFromString.get("durian"));
+      assertEquals("x-j", recordingFromString.get("jalapeno"));
+    }
+  }
+}
